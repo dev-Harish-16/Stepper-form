ffcf3e1f6c281549d1c7a96306ed157f5da25b73
Harish <99942242+dev-Harish-16@users.noreply.github.com>
2023-01-04 20:17:14 +0530

    Update feedBackApi.js

--- Api/feedBackApi.js
+++ Api/feedBackApi.js
@@ -8,15 +8,13 @@ const multer = require('multer');
 
 // **nodeMailer configuration
 const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
+    service: 'gmail',
     auth: {
-        user: 'william.abernathy63@ethereal.email',
-        pass: 'bgvxW9dsKsf2fwXUZy'
+        user: process.env.USER,
+        pass: process.env.PASS
     }
 });
 
-// **multer storage configuration
 const storage = multer.diskStorage({
     destination: (req, file, callBack) => {
         callBack(null, 'uploads/')
@@ -46,16 +44,19 @@ feedBackApp.post('/comments', upload.array('files', 5), expressAsyncHandler(asyn
 
     // ** mail Trigger - if reply is "yes"
     if (data.reply == "yes") {
-        console.log(data.email);
+        // console.log(data.email);
         transporter.sendMail({
-            from: 'layne.thompson@ethereal.email', // sender address
+            from: process.env.USER, // sender address
             to: data.email, // list of receivers
             subject: "Thanks for your feedback!", // Subject line
-            text: "We apperciate your feedback, and thank you for flying Delta", // plain text body
-            html: "<h1>We apperciate your feedback, and thank you for flying Delta</h1>", // html body
+            text: "We appreciate your feedback, and thank you for flying Delta", // plain text body
+            html: `<h4>Dear ${data.firstName},</h4>
+                    <h1>We appreciate your feedback, and thank you for flying with us</h1>
+                    <img src="https://www.shutterstock.com/image-photo/passengers-commercial-airplane-flying-above-260nw-1573918030.jpg">`, // html body
         });
 
     }
+
     const saveData = await new FeedBack(data).save()
     res.send({ message: "Data Saved Successsfully", payload: saveData })
 }))
